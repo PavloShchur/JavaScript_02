691f33076a74c5712f9e2ddf8300eec36927d7d6
Pavlo Shchur <pavloshchur@gmail.com>
2018-03-13 11:17:41 +0200

    JavaScript: FocusAndBlur: script.js is modified.

--- events/FocusAndBlur/script.js
+++ events/FocusAndBlur/script.js
@@ -1,5 +1,6 @@
-
-var emailField = document.getElementById("email");
+let emailField = document.getElementById("email");
+let nameField = document.getElementById("name");
+let tourField = document.getElementById("tourID");
 
 emailField.onfocus = function() {
 	if ( emailField.value == "your email") {
@@ -11,4 +12,28 @@ emailField.onblur = function() {
 	if ( emailField.value == "") {
 		emailField.value = "your email";
 	}
+};
+
+nameField.onfocus = function() {
+    if ( nameField.value == "your name") {
+        nameField.value = "";
+    }
+};
+
+nameField.onblur = function() {
+    if (nameField.value == "") {
+        nameField.value = "your name";
+    }
+};
+
+tourField.onfocus = function() {
+    if (tourField.value == "your tour") {
+        tourField.value = "";
+    }
+};
+
+tourField.onblur = function() {
+    if (tourField.value == "") {
+        tourField.value = "your tour";
+    }
 };
